f80e27d15a071c7f63d3237972b4f33eb274e343
ahmad_sakour <hussian0938605821@gmail.com>
2024-11-17 14:26:55 +0300

    add localiztaions to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:medsync/core/constant/size_config.dart';
 import 'package:medsync/core/themes/app_themes.dart';
 import 'package:medsync/features/app_layout/presentation/view/app_layout_view.dart';
+import 'package:medsync/generated/l10n.dart';
 
 void main() {
   runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
@@ -14,6 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
